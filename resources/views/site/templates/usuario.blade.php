b16552011bcbee89893b572892c341edbbfe1a0b
Miqueias Fernando <miqueiasfernando@gmail.com>
2021-05-11 23:28:55 -0300

    melhorias

--- resources/views/site/templates/usuario.blade.php
+++ resources/views/site/templates/usuario.blade.php
@@ -1,38 +1,39 @@
 @extends('site.templates.principal')
 @section('principal')
-@push('scriptsHead')
-<link rel="stylesheet" type="text/css" href="css/usuario.css">
-@endpush
+  @push('scriptsHead')
+  <link rel="stylesheet" type="text/css" href="css/usuario.css">
+  @endpush
+   
+    
+    <div class="sidebarUser col-md-2 col-md-offset-1">
+      <div class="col-md-12 userButtons">
+        <h2>STATUS DA SUA CONTA</h2>
+        <dl class="pontosUser"><br>
+          <dt>Atualmente você tem:</dt>
+          <dd><span>80</span><br>
+          Pontos Disponíveis.</dd>
+        </dl>
+        <!-- Botoes conta -->
+        <nav class="menu-user-btn">
+          <ul>
+            <li>
+              <a href="/vouchers">
+                VOUCHERS ATIVOS
+              </a>
+            </li>
+            <li>
+              <a href="/minha_conta">
+                MINHA CONTA
+              </a>
+            </li>
+            <li><a href="/meus-vouchers">MEUS VOUCHERS</a></li>
+          </ul>
+        </nav>
+      </div>
+    
 
-<div class="container-fluid UserContentMain">
-  <div class="sidebarUser col-md-2 col-md-offset-1">
-    <h2>STATUS DA SUA CONTA</h2>
-    <div class="col-md-12 userButtons">
-      <dl class="pontosUser">
-        <dt>Atualmente você tem:</dt>
-        <dd><span></span><br>
-        Pontos Disponíveis.</dd>
-      </dl>
-      <!-- Botoes conta -->
-      <nav class="menu-user-btn">
-        <ul>
-          <li>
-            <a href="/vouchers">
-              VOUCHERS ATIVOS
-            </a>
-          </li>
-          <li>
-            <a href="/minha_conta">
-              MINHA CONTA
-            </a>
-          </li>
-          <li><a href="/meus-vouchers">MEUS VOUCHERS</a></li>
-        </ul>
-      </nav>
-    </div>
-  </div>
   
   @push('scriptsFooter')
 
   @endpush
-  @endsection
+@endsection
