8dc566e593af487e3927ef7bb25d5b6fdbec45f5
Miqueias08 <miqueiasfernando@gmail.com>
2021-05-18 14:20:23 -0300

    SS

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\cooperativas;
+use DB;
 
 class ApiController extends Controller
 {
