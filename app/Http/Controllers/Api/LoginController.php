afb2bb328d8025f434bc3ebd81c9513810ea798f
Miqueias08 <miqueiasfernando@gmail.com>
2021-05-19 15:58:34 -0300

    hash

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\usuarios;
+use Hash;
 
 class LoginController extends Controller
 {
