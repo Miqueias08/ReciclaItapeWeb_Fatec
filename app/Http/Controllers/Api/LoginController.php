34277af9d4ce4461efb3681080822a017fea219e
Miqueias08 <miqueiasfernando@gmail.com>
2021-05-19 16:00:39 -0300

    sss

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -17,7 +17,7 @@ class LoginController extends Controller
         	$senhaB = $senhaBD->senha;
             if(Hash::check($senha,$senhaB)){
             	$id = usuarios::where('email',$email)->select('id_usuario')->first();
-                $user = usuarios::find($id->id_usuario)->except("senha");
+                $user = usuarios::find($id->id_usuario);
                 return json_encode(["status"=>"ok","dados"=>$user]);
             }
             else{
